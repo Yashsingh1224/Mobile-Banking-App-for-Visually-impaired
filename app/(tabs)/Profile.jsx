b6038859aae18fd977774f6408f6a3f063a842e2
Yash singh <yk73690932@gmail.com>
2025-03-10 23:23:49 +0530

    First Commit

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { View, Text } from "react-native";
-import { Link, router } from "expo-router";
+import { Link } from "expo-router";
 
 const Profile = () => {
-    return (
-        <View className="w-full justify-end items-center pt-3 flex-row">
 
-            <Link href="/Login" className="text-lg text-secondary mx-2">LogOut</Link>
-        </View>
+    return (
+        <Link href="/Login" className="text-lg text-secondary mx-2">Login</Link>
     )
 }
 export default Profile
